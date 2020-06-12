cc0e6ff358b1fa22385ab0ccac7250d27d4ff81e
Lucas Federico Cari <lucasfedericocari@gmail.com>
2020-06-11 21:34:40 -0300

    incio

---
+++ JS/28_modulos_JS.js
@@ -0,0 +1,9 @@
+import saludar, { PI } from "./constantes.js"
+import { aritmetica as calculos } from "./aritmetica.js"
+
+console.log('Achivos modulos.JS');
+console.log(PI);
+console.log(calculos.sumar(4,5));
+console.log(calculos.restar(4,5));
+
+saludar();
---
+++ JS/constantes.js
@@ -0,0 +1,9 @@
+export const PI = Math.PI;
+
+let usuario = 'Lucas';
+let pasword = 'qwerty';
+
+
+export default function saludar() {
+    console.log('Hola módulos +ES6')
+}
